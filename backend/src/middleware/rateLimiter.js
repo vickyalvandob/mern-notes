ff2e9427e6572efb0dde095b15357ecc7aebd51a
vickyalvandob <73622790+vickyalvandob@users.noreply.github.com>
2025-06-03 17:00:19 +0700

    add middleware and rate limit

---
+++ backend/src/middleware/rateLimiter.js
@@ -0,0 +1,20 @@
+import ratelimit from "../config/upstash.js";
+
+const rateLimiter = async (req, res, next) => {
+  try{
+    const {success} = await ratelimit.limit("my-limit-key")
+
+    if(!success) {
+      return res.status(429).json({
+        message: 'Too many requests, please try again later.'
+      });
+    }
+    
+    next();
+  } catch (error) {
+    console.error('Rate limiter error:', error);
+    next(error);
+  }
+}
+
+export default rateLimiter;
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import notesRoute from './routes/notesRoute.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
+import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 
@@ -9,11 +10,15 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
+app.use(rateLimiter);
 
-connectDB();
 
 app.use("/api/notes", notesRoute);
 
-app.listen(PORT, () => {
-  console.log('Server is running on port:', PORT);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log('Server is running on port:', PORT);
+  });
 });
+
