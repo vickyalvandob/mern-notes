1cb7d62de9f055e34a1e3762f0c6b7d9c366f133
vickyalvandob <73622790+vickyalvandob@users.noreply.github.com>
2025-06-08 14:05:14 +0700

    create notes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import RateLimitedUI from '../components/RateLimitedUI';
-import axios from "axios"
 import toast from 'react-hot-toast';
 import NoteCard from '../components/NoteCard';
+
+import api from "../lib/axios"
 
 function HomePage() {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -13,7 +14,7 @@ function HomePage() {
   useEffect(() => {
     const fetchNotes = async () => {
       try{
-        const res = await axios.get("http://localhost:5001/api/notes");
+        const res = await api.get("/notes");
         console.log(res.data);
         setNotes(res.data)
         setIsRateLimited(false)
